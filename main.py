807de7a30e2496118b207199fe93b95757c4a704
Ujjwal Ayyangar <ujjwal.097@gmail.com>
2019-11-08 15:08:10 -0500

    Deployment fixes.
    Syncing local db with server

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from app import app
-from db_setup import setup
-setup()
+
 
 if __name__ == '__main__':
     app.debug = True
-    setup()
     app.run(debug=True, host='0.0.0.0', port=5000)
     #app.run(host='127.0.0.1', port=5000)
